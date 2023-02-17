b317f4051d1642387546456e6e57640bc7f20aa2
adebowale <adesoyeadebowale@yahoo.com>
2023-02-16 16:07:32 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,11 @@
 
 /**
  * main - Entry point
- * Description: prints "\"Programming is like building a multilingual puzzle"
- * Return: 0
+ *
+ * Return: Always 0 (Success)
  */
-
-
-int main(void) {
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
