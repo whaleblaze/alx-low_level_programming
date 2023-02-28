4f9c28bc07c6794016b8a7f085e51ae1217657ab
adebowale <adesoyeadebowale@yahoo.com>
2023-02-28 14:35:06 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* puts2 - print every char
+*@str: print char
+*
+* Return: Always 0.
+*/
+void puts2(char *str)
+{
+int a;
+
+for (a = 0; str[a] != '\0'; a++)
+{
+if (a % 2 == 0)
+_putchar(str[a]);
+}
+
+_putchar('\n');
+}
