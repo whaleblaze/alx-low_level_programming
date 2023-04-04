77beef80993837f6d6601d5852d64a1f65c46524
adebowale <adesoyeadebowale@yahoo.com>
2023-04-03 17:40:09 -0700

    print-list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,21 +9,21 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-        {
-            printf("[0] (nil)\n");
-        }
-        else
-        {
-            printf("[%d] %s\n", h->len, h->str);
-        }
-        count++;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
+	}
 
-    return (count);
+	return (count);
 }
