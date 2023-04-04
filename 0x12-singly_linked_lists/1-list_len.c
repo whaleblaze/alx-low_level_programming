2ce7eb452162c673c57ac0383cf9fe5baff79619
adebowale <adesoyeadebowale@yahoo.com>
2023-04-03 18:02:28 -0700

    list len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -11,12 +11,13 @@
  */
 size_t list_len(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
-    return (count);
+	while (h != NULL)
+		{
+			count++;
+			h = h->next;
+		}
+
+	return (count);
 }
