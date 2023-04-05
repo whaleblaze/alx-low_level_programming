19a3c4b99569247a9679933efae5a7afd2a14821
adebowale <adesoyeadebowale@yahoo.com>
2023-04-04 19:31:37 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
  *
  * Return: address of the new node, or NULL if it failed
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *new_node, *current;
 	unsigned int i;
