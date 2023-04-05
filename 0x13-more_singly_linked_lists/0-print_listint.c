cbba99766e2572ba56662a67f6747b68737cc5f2
adebowale <adesoyeadebowale@yahoo.com>
2023-04-04 17:44:56 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,12 +4,9 @@
 #include <string.h>
 
 /**
-
-print_listint - prints all the elements of a listint_t list
-
-@h: pointer to head of list
-
-Return: number of nodes
+*print_listint - prints all the elements of a listint_t list
+*@h: pointer to head of list
+*Return: number of nodes
 */
 size_t print_listint(const listint_t *h)
 {
